4a6c73d8a8663027d00a47b8acd9953d848a7528
suhani-212004 <s11723210027@gmail.com>
2025-08-08 17:52:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Node.js!');
+  res.send('Hello from technova');
 });
 
 app.listen(PORT, () => {
